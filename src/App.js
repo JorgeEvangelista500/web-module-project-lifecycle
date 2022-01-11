722ebb30c5adbb24440c8a3ce7fe57c181c3b663
Jorge <jorgeevangelista500@gmail.com>
2022-01-11 18:33:53 -0500

    finished setting up user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import axios from 'axios';
 import React from 'react';
+import User from './components/User';
 
 class App extends React.Component {
+  state = {
+    user: [],
+    followers: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/JorgeEvangelista500`)
+        .then(resp=> {
+          this.setState({
+            ...this.state,
+            user: resp.data
+          })
+        })
+  }
+
   render() {
     return(<div>
-      Github Card
+      <h1>GITHUB INFO</h1>
+      <form>
+          <input/>
+          <button>Search</button>
+      </form>
+      <div>
+        <User user={this.state.user}/>
+      </div>
     </div>);
   }
 }
---
+++ src/components/User.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { render } from "react-dom";
+
+class User extends React.Component {
+    render() {   
+    return(
+    <div>
+        <img width="200" src={this.props.user.avatar_url} alt="avatar"/>
+        <div>
+           <h2> {this.props.user.name}</h2>
+            <p>{this.props.user.company}</p>
+            <p> `Total Repos:{this.props.user.public_repos}</p>
+            <p> Total followers:{this.props.user.followers}</p>
+        </div>
+    </div>)
+    }
+}
+    export default User;
